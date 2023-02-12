b2f147b27c648bfac53b4cd1cdde5501a1aa2116
bentharwood <122299459+bentharwood@users.noreply.github.com>
2023-02-11 22:00:44 -0500

    Update ch5_ex9.sql

--- ch5_ex9.sql
+++ ch5_ex9.sql
@@ -1,2 +1,2 @@
-DELETE FROM invoice_line_items WHERE invoice_id = 120;
-DELETE FROM invoices WHERE invoice_id = 120
+DELETE FROM invoice_line_items WHERE invoice_id = 115;
+DELETE FROM invoices WHERE invoice_id = 115
